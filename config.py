e8a9ff3a32680ac0a76c7ce1b0d3ed69921091c6
Alokchauhan11 <153092293+Alokchauhan11@users.noreply.github.com>
2024-10-04 22:16:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://alokchauhanutuber:whCrUDGzlu4GUbRx@cluster0.d9quk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "alokchauhanutuber")
+DB_URI = environ.get("DB_URI", "mongodb+srv://frankanimes:qayplmwsxokn@cluster0.xksjqdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "Cluster0")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
